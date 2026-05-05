e20aef14a22459625346c79cce037c08697cdd14
DuskyElf <duskyelf.dev+git@gmail.com>
2026-05-05 14:15:26 +0530

    chore: project setup and initial content

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Develop my brain with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+              pkgs.prettierd
+              pkgs.typescript-language-server
+              pkgs.vscode-langservers-extracted
+            ];
+          };
+        }
+      );
+    };
+}
